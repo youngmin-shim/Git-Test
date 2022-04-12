f69909ebb44dd3c752df2adcbcf09d055e12d0be
youngmin-shim <ym.sim@cardoc.kr>
2022-04-12 15:09:54 +0900

    rebase

--- GitTest/GitTest/GitTest/ViewController.swift
+++ GitTest/GitTest/GitTest/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("두번째 커밋")
         print("sample1")
+<<<<<<< HEAD
         print("main 세번째 커밋")
+=======
+        print("sample2")
+>>>>>>> db4d5f4 (sample1 second commit)
     }
 
 
