336a2e566b5b8efc0e3a5fa042d8c3b110112e73
youngmin-shim <ym.sim@cardoc.kr>
2022-04-12 21:27:14 +0900

    Revert "interactive-rebase 2"
    This reverts commit b25f510019be09bbe1391dd6398b0daa3c7478f0.

--- GitTest/GitTest/GitTest/SceneDelegate.swift
+++ GitTest/GitTest/GitTest/SceneDelegate.swift
@@ -27,7 +27,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
